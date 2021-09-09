dc861122ce369f2515c03ca2c7dc2cd1e00ceb32
Plontz <90393834+Plontz@users.noreply.github.com>
2021-09-09 08:19:39 -0400

    Create Critter.cpp

---
+++ Critter.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Critter.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+ACritter::ACritter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	RootComponent=CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    MeshComponent=CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera")); //Crear componente de la Camara
+	Camera->SetupAttachment(GetRootComponent()); // Agregar componente de la camara en root
+	Camera->SetRelativeLocation(FVector{-300.f, 0.f, 300.f});
+	Camera->SetRelativeRotation(FRotator(-45.f,0.f,0.f));
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0; // Tomar el control del jugador
+	CurrentVelocity=FVector{0.F};
+	MaxSpeed =100.f;
+}
+
+// Called when the game starts or when spawned
+void ACritter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACritter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime); // Correra un 100 % en resultado
+	SetActorLocation(NewLocation);
+
+}
+
+// Called to bind functionality to input
+void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveFoward"), this,&ACritter::MoveFoward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this,&ACritter::MoveRight);
+}
+
+void ACritter::MoveFoward(float Value)
+{
+	CurrentVelocity.X=FMath::Clamp( Value, -1.f, 1.f) * MaxSpeed; // El clamp toma valor minimo y maximo.
+}
+
+void ACritter::MoveRight(float Value) 
+{
+	CurrentVelocity.Y=FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
